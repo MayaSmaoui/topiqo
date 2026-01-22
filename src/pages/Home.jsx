7c595f772ee3aa2a8c115c388c2dfa54a5856b5a
MayaSmaoui <maya.smaoui17@gmail.com>
2026-01-22 01:03:53 +0000

    Add button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import FeaturesShowcase from '../FeaturesShowcase';
 import topiGif from '../assets/topiqo-wave-blank.gif';
 
 export default function Home({ t, lang }) {
+  const appStoreLink = "https://apps.apple.com/gb/app/topiqo/id6748356744";
+
   return (
     <>
       {/* HERO */}
@@ -11,7 +13,15 @@ export default function Home({ t, lang }) {
           <img src={topiGif} alt="Topi animé" className="topi-img" />
           <h1>{t.heroTitle}</h1>
           <p>{t.heroText}</p>
-          <button className="download-button">{t.download}</button>
+
+          <a
+            href={appStoreLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="download-button"
+          >
+            {t.download}
+          </a>
         </div>
         <div className="hero-bg" aria-hidden="true"></div>
       </main>
